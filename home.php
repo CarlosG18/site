55ef38282d5adb0a7005cd5f47143e7b336d3d43
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-06-15 17:32:46 +0000

    header_home = botao add cat e show cat

--- home.php
+++ home.php
@@ -1,22 +1,36 @@
 <?php
-    session_start();
-    include 'includes/conexao.php';
-if($_SESSION["logado"] == true){
+  session_start();
+  include 'includes/conexao.php';
+  if($_SESSION["logado"] == true){
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>home</title>
-<?php
+  <?php
     include 'includes/head.php';
     include 'includes/header_home.php';
-?>
+  ?>
 </head>
 <body>
-
+  <?php
+    if($papel[0] == 1){ //parte do cliente
+  ?>
+    <div class="container">
+      bem vindo <?php echo "".$_SESSION["nome"] ?> cliente
+    </div>
+  <?php  }else if($papel[0] == 2){ //parte do barbeiro
+  ?>
+    <div class="container">
+    barbeiro
+    </div>
+  <?php
+    }
+  ?>
 </body>
-</html>
-<?php }else{
-  echo "<script>document.location='index.php'</script>";
-}
-?>
+</html> 
+  <?php 
+  }else{
+    echo "<script>document.location='index.php'</script>";
+  }
+  ?>
--- includes/header_home.php
+++ includes/header_home.php
@@ -1,3 +1,16 @@
+<?php
+  $sql = "SELECT papel_id FROM usuario_papel WHERE usuario_id=".$_SESSION["id"];
+  $result = pg_query($conexao, $sql);
+  $papel = pg_fetch_row($result,$i);
+  $string_papel;
+
+  if($papel[0] == 1){
+    $string_papel = "cliente";
+  }else if($papel[0] == 2){
+    $string_papel = "barbeiro";
+  }
+?>
+
 <header>
      <nav id="nav" class="navbar navbar-expand-md bg-light navbar-light shadow">
      <!-- Brand -->
@@ -21,6 +34,17 @@
       <li class="nav-item">
         <a class="nav-link" href="#">ajuda</a>
       </li>
+    </ul>
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-link">
+        <?php echo $string_papel; ?>
+      </li>
+      <li>
+        <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg> categoria</a>
+      </li>      
     </ul>
     <ul class="navbar dropdown ml-auto mb-0">
        <button type="button" class="btn btn-light shadow dropdown-toggle" data-toggle="dropdown">
