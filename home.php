5df7ce0663ffe406574aa69de212c3ac2ff9ac80
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-06-26 00:12:02 +0000

    Add_pag_cliente

--- home.php
+++ home.php
@@ -16,14 +16,60 @@
   <?php
     if($papel[0] == 1){ //parte do cliente
   ?>
-    <div class="container">
-      bem vindo <?php echo "".$_SESSION["nome"] ?> cliente
-    </div>
+    <main>
+      <div id="box_destaque" class="container d-flex shadow mx-auto my-3 p-3">
+         <div class="container">
+           <img style="max-width: 200px; width:150px; padding-top: auto" src="pesquisa.svg" alt="pesquisar">
+         </div>
+         <div class="container d-flex flex-column text-center">
+           <p 
+id="destaque" class="container">Marque seu horário já</p>
+           <div class="container">
+           <a class="container" href="#"> <button type="button" class="btn btn-primary shadow mx-auto">Clique aqui</button></a>
+           </div>
+         </div>
+      </div>
+
+      <div class="container mt-5">
+        <h1 id="destaque">Seus agendamentos ativos</h1>
+        <div id="line"></div>
+        <div class="container">
+          <p>Sem agendamentos ativos no momento</p>
+        </div>
+      </div>
+
+      <div class="container mt-5">
+        <h1 id="destaque">Seus agendamentos passados</h1>
+        <div id="line"></div>
+        <div class="container">
+          <p>você não possui agendamentos passados</p>
+        </div>
+      </div>
+
+      <div class="container mt-5">
+        <h1 id="destaque">Progressos em Promoções</h1>
+        <div id="line"></div>
+        <div class="container">
+          <p>Sem promoções ativadas</p>
+        </div>
+      </div>
+    </main>
   <?php  }else if($papel[0] == 2){ //parte do barbeiro
   ?>
-    <div class="container">
-    barbeiro
-    </div>
+    <main>
+      <div id="box_destaque" class="container d-flex shadow mx-auto my-3 p-3">
+         <div class="container">
+           <img style="max-width: 200px; width:150px; padding-top: auto" src="tesoura.svg" alt="pesquisar">
+         </div>
+         <div class="container d-flex flex-column text-center">
+           <p 
+id="destaque" class="container">Cadastre sua Barbearia já!</p>
+           <div class="container">
+           <a class="container" href="cadastro.php"> <button type="button" class="btn btn-primary shadow mx-auto">Clique aqui</button></a>
+           </div>
+         </div>
+      </div>
+    </main>
   <?php
     }
   ?>
